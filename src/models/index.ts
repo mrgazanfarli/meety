d26ce843ca290a02e47afb8c4f9d9253544c7faf
mrgazanfarli <mr.gazanfarli@gmail.com>
2020-12-27 03:02:05 +0400

    error resolution

--- src/models/index.ts
+++ src/models/index.ts
@@ -87,7 +87,7 @@ export interface IEvent {
     noiseAllowed: boolean;
     smokingAllowed: boolean;
     createdBy: string;
-    eventStatus: EEventStatus;
+    eventStatus?: EEventStatus;
     imgUrl?: string;
 }
 
